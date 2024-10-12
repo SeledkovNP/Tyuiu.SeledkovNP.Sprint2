86d2f4ea5f47696bbfe0712f2968b2f345de1e7e
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-10-12 11:00:53 +0500

    Task3

---
+++ Tyuiu.SeledkovNP.Sprint2.Task3.V26.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.SeledkovNP.Sprint2.Task3.V26.Lib
+{
+    public class DataService : ISprint2Task3V16
+    {
+        public double Calculate(double x)
+        {
+
+            double y;
+            if (x > 0) y = x + Math.Pow((x + 1) / (Math.Cos(Math.Pow(x, 2))), x);
+            else if (x == 0) y = (15 + x) / (3 * 3 - Math.Sin(Math.Pow(x, 2)));
+            else if (x > -30 && x < 0) y = Math.Pow(Math.Sin(Math.Pow(x, 3)) / (x + 5), x);
+            else y = x + 7 * x - (1 / x);
+            return Math.Round(y, 3);       // 3 цифры после запитой
+
+        }
+    }
+}
---
+++ Tyuiu.SeledkovNP.Sprint2.Task3.V26/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.SeledkovNP.Sprint2.Task3.V26.Lib;
+namespace Tyuiu.SeledkovNP.Sprint2.Task3.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Селедков Н. П. | Смартб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Селедков Никита Павлович | Смартб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+            Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+            Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение до    *");
+            Console.WriteLine("* трех знаков после запятой                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            // ФОРМУЛА!!!   
+
+            Console.WriteLine("Введите переменную X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+          
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.Calculate(x);
+            Console.WriteLine("Y=" + res);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
