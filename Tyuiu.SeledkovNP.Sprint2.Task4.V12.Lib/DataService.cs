d808f2ae99263df7ce568607aaee789f38fc329c
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-10-13 18:53:26 +0500

    Task4

---
+++ Tyuiu.SeledkovNP.Sprint2.Task4.V12.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.SeledkovNP.Sprint2.Task4.V12.Lib
+{
+    public class DataService : ISprint2Task4V12
+    {
+        public double Calculate(double x, double y)
+        {
+            // ':' это разделения условий                    '/' это деление
+            
+            //                               условие:              первое                                                                             второе
+            double z = Math.Sqrt(x) > y * 2    ?     Math.Pow(7 + 2 / Math.Pow(y, 2), x)    :     (Math.Pow(3 * x, x) - Math.Pow(Math.Cos(y), 2) + 10) / (Math.Pow(y, 2) - Math.Pow(Math.Sin(x), 2) + 12);
+            return Math.Round(z, 3);
+
+        }
+    }
+}
---
+++ Tyuiu.SeledkovNP.Sprint2.Task4.V12/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.SeledkovNP.Sprint2.Task4.V12.Lib;
+namespace Tyuiu.SeledkovNP.Sprint2.Task4.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Селедков Н. П. | Смартб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Селедков Никита Павлович | Смартб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Сделать всё по фото                                                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            // ФОРМУЛА!!   
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Ваш ответь  = " + res);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
