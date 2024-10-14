49be2e9c5c446ecb69bebdc4bf189bd28a06618f
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-10-14 09:16:36 +0500

    Fix

--- Tyuiu.SeledkovNP.Sprint2.Task5.V8.Lib/DataService.cs
+++ Tyuiu.SeledkovNP.Sprint2.Task5.V8.Lib/DataService.cs
@@ -5,46 +5,48 @@ namespace Tyuiu.SeledkovNP.Sprint2.Task5.V8.Lib
     {
         public string FindDateOfPreviousDay(int m, int n)
         {
+            
+           
+                string res;
+                switch (n)
+                {
+                    case 1:
+                        switch (m)
+                        {
+                            case 1:
+                                n = 31;
+                                m = 12;
+                                break;
+                            default:
+                                m = m - 1;
+                                switch (m)
+                                {
+                                    case 1:
+                                    case 3:
+                                    case 5:
+                                    case 7:
+                                    case 8:
+                                    case 10:
+                                        n = 31;
+                                        break;
+                                    case 2:
+                                        n = 28;
+                                        break;
+                                    default:
+                                        n = 30;
+                                        break;
+                                }
+                                break;
+                        }
+                        break;
+                    default:
+                        n = n - 1;
+                        break;
+                }
+                res = ($"{n}.{m}");  //сам ответь
+                return res;
 
-            string res;
-            switch (n)
-            {
-                case 1:
-                    switch (m)
-                    {
-                        case 1:
-                            n = 31;
-                            m = 12;
-                            n--;
-                            break;
-                        default:
-                            m--;
-                            switch (m)
-                            {
-                                case 1:
-                                case 3:
-                                case 5:
-                                case 7:
-                                case 8:
-                                case 10:
-                                    n = 31;
-                                    break;
-                                case 2:
-                                    n = 28;
-                                    break;
-                                default:
-                                    n = 30;
-                                    break;
-                            }
-                            break;
-                    }
-                    break;
-                default:
-                    n--;
-                    break;
-            }
-            res = "" + m + n;
-            return res;
+            
 
         }
     }
