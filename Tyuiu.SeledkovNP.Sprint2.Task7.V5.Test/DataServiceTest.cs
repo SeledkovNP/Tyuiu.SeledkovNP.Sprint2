8f075cf0b2ad585c77f3ff96df12c29292bb0727
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-10-15 23:24:19 +0500

    Task7

---
+++ Tyuiu.SeledkovNP.Sprint2.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+using Tyuiu.SeledkovNP.Sprint2.Task7.V5.Lib;
+namespace Tyuiu.SeledkovNP.Sprint2.Task7.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            DataService ds = new DataService();
+
+            double x = 0.1;
+            double y = 0.1;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
